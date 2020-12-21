821603946c6afc8bbbfd238c583b7dee43afa7da
ledmega <ledmega@192.168.200.130>
2020-12-21 16:13:08 +0900

    dd

--- src/com/app/ApplicationMain.java
+++ src/com/app/ApplicationMain.java
@@ -1,10 +1,27 @@
 package com.app;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class ApplicationMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hell");
+		//System.out.println("hell");
+		
+		String hostName = null;
+		try
+		{
+			final InetAddress localhost = InetAddress.getLocalHost();
+			hostName = localhost.getHostName();
+		}
+		catch (final UnknownHostException e)
+		{
+			hostName = "";
+		}
+		
+		System.out.println("hostname = " + hostName);
+		
 	}
 
 }
